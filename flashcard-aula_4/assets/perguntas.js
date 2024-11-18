d95aa3405786fe9e32760729c607c0501c377684
Lizfernandas2 <144147635+Lizfernandas2@users.noreply.github.com>
2024-11-18 11:48:13 -0300

    projeto finalo!!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ flashcard-aula_4/assets/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de Programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a Capital da França?',
+    'A Capital da França é Paris'
+)
+
+criaCartao(
+    'Programação'
+    'O que é uma função?'
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'linguagem Inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em inglê é HI (RAI)'
+)
